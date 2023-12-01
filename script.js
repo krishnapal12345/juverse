382d25455775219be97d07e2ba4a64cc11522b7d
Parth Manocha <132323562+Eordinary01@users.noreply.github.com>
2023-12-01 23:24:13 +0530

    added resposiveness!!But need to add more

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+burger = document.querySelector('.burger');
+navbar = document.querySelector('.navbar');
+juverse = document.querySelector('.juverse');
+JuVerse1 = document.querySelector('.JuVerse1');
+navcontent = document.querySelector('.navcontent');
 
+
+
+
+
+burger.addEventListener('click',()=>{
+    JuVerse1.classList.toggle('v-class-resp');
+    juverse.classList.toggle('v-class-resp');
+    navcontent.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+
+})
